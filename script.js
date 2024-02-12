65458c5ad132256c6bdfe01bbcc908cf004efd65
Safia Raza <safiafeyiaz@gmail.com>
2024-02-12 12:01:58 +0100

    My first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondEl = document.getElementById('seconds');
+
+const newYears = "1 Jan 2025"
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds % 60);
+    console.log(days, hours, mins, seconds);
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondEl.innerHTML = seconds;
+}
+        countdown();
+    setInterval(countdown, 1000);
